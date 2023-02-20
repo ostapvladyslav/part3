72f2fd4b61bf4ffeb11a264ec72410b1f402887b
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-20 18:04:02 +0200

    Exercise 3.3

--- index.js
+++ index.js
@@ -24,15 +24,26 @@ const persons = [
   },
 ];
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
-});
-
 app.get('/info', (req, res) => {
   res.end(`
   <p>Phonebook has info for ${persons.length} people</p>
   <p>${new Date()}</p>
   `);
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001;
