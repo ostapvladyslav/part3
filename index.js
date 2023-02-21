d4d5614d3d357500c82f69a2f326dbc64bbdc912
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-21 02:02:46 +0200

    Exercise 3.14

--- index.js
+++ index.js
@@ -32,35 +32,6 @@ app.use(
   })
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-function generateId() {
-  const min = Math.ceil(0);
-  const max = Math.floor(10000);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
 app.get('/info', (req, res) => {
   res.end(`
   <p>Phonebook has info for ${persons.length} people</p>
@@ -80,37 +51,29 @@ app.get('/api/persons/:id', (req, res) => {
   });
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
-});
-
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
+  if (body.name === undefined || body.number === undefined) {
+    return json.status(400).json({
       error: 'name or number missing',
     });
   }
 
-  if (persons.filter((person) => person.name === body.name).length > 0) {
-    return res.status(400).json({
-      error: 'name must be uniques',
-    });
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then((newPerson) => {
+    res.json(newPerson);
+  });
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  // const id = Number(req.params.id);
+  // persons = persons.filter((person) => person.id !== id);
+  // res.status(204).end();
 });
 
 app.use(unknownEndpoint);
